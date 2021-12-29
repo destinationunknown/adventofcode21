aa819503e782c54df6bd6a85b274fa9f88ab7c77
Varun Ramakrishnan <ravarun3@gmail.com>
2021-12-29 12:21:01 -0800

    Solved Day 8

--- 8.py
+++ 8.py
@@ -27,7 +27,62 @@ def part_two(data):
 
     for line in data:
         in_segments, out_segments = line
-    pass
+        # Using frozenset because it can be used as a key for a dict
+        in_segments = [frozenset(list(segment)) for segment in in_segments]
+        # Put the combinations we know at the front
+        in_segments = list(sorted(in_segments, key=lambda x: len(x) not in (2, 3, 4, 7)))
+        out_segments = [frozenset(list(segment)) for segment in out_segments]
+
+        # If we know 1 and 4, we can figure out the rest
+        # We basically need to find a unique identifier for each combination of segments
+        # 1, 4, 7, 8 have a unique number of segments
+        known_lens = {
+            2: 1,
+            3: 7,
+            4: 4,
+            7: 8
+        }
+        unique_nums = {}
+        known_nums = {}
+        for segment in in_segments:
+            if segment not in known_nums:
+                if len(segment) in known_lens:
+                    unique_nums[known_lens[len(segment)]] = segment
+                    known_nums[segment] = known_lens[len(segment)]
+                else:
+                    # We need to figure these out using the known segment sets (sets with unique lengths)
+                    # length 6: 0, 6, 9
+                    # if 1's set is not in the set, then it is 6
+                    # if 4's set is in the set, then it is 9
+                    # else, it is 0
+                    if len(segment) == 6:
+                        if not unique_nums[1].issubset(segment):
+                            known_nums[segment] = 6
+                        elif unique_nums[4].issubset(segment):
+                            known_nums[segment] = 9
+                        else:
+                            known_nums[segment] = 0
+
+                    # length 5: 2, 3, 5
+                    # if 1's set is in the set, then it is 3
+                    # if (8's set - 4's set) is in the set, then it is 2
+                    if len(segment) == 5:
+                        if unique_nums[1].issubset(segment):
+                            known_nums[segment] = 3
+                        elif (unique_nums[8] - unique_nums[4]).issubset(segment):
+                            known_nums[segment] = 2
+                        else:
+                            known_nums[segment] = 5
+
+        cur_sum = 0
+        ten_pow = 1
+        for segment in list(reversed(out_segments)):
+            cur_sum += known_nums[segment] * ten_pow
+            ten_pow *= 10
+
+        res += cur_sum
+
+    return res
 
 print(part_one(data))
 print(part_two(data))
